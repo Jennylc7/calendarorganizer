fac34fc5f6b81e05cf9e2b04af0dc8cda92a12e7
jennylc7 <thepilatesnut@gmail.com>
2021-05-01 13:24:41 -0600

    calendar commit

--- script.js
+++ script.js
@@ -10,30 +10,94 @@
 //THEN each time block is color-coded to indicate whether it is in the past, present, or future
 
 //Button
+
+//the document is "ready" for the code inside to run
 $(document).ready(function () {
 //WHEN I click into a time block
+
+//execute the javascript
   $("button").click(function () {
     //  $(this).hide();
-    alert("The button was clicked")
+    alert("Save your note")
     //save button
     //var user input
-    $(this).sibling(".schedule").val().trim() --> 
+
+    // Return all sibling elements of each <li> element with class name
+   var value =  $(this).sibling(".schedule").val().trim();
+
+   //The parent() method returns the direct parent element of the selected element.
+   var time = $(this).parent().attr("id");
+
+     // save in localStorage
+    //  localStorage.setItem(time, value);
+    // });
+
     //".schedule" save it in a variable
     //var timeRow = 
-    $(this).parent().att("data-hour") --> 
+
+    var time = $(this).parent().att("data-hour") --> 
     //tells you which time block you're on 
     window.localStorage.setItem(timeRow, todoInput);
   });
   
-  
+  function hourUpdater() {
+    // get current number of hours
+    var currentHour = moment().hours();
+
+    // loop over time blocks
+    $(".time-block").each(function() {
+      var blockHour = parseInt($(this).attr("id").split("-")[1]);
+
+      // check if we've moved past this time
+      if (blockHour < currentHour) {
+        $(this).addClass("past");
+      } 
+      else if (blockHour === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      } 
+      else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
+    });
+  }
+
+  hourUpdater();
+
+
+
+
+
+
+
 //make one of these for each time
   $("#7 .schedule").val(localStorage.getItem(""))
   $("#8 .schedule").val(localStorage.getItem(""))
   $("#9 .schedule").val(localStorage.getItem(""))
   $("#10 .schedule").val(localStorage.getItem(""))
   $("#11 .schedule").val(localStorage.getItem(""))
-  $("#9 .schedule").val(localStorage.getItem(""))
+  $("#12 .schedule").val(localStorage.getItem(""))
+  $("#13 .schedule").val(localStorage.getItem(""))
+  $("#14 .schedule").val(localStorage.getItem(""))
+  $("#15 .schedule").val(localStorage.getItem(""))
+  $("#16 .schedule").val(localStorage.getItem(""))
+  $("#17 .schedule").val(localStorage.getItem(""))
+  $("#18 .schedule").val(localStorage.getItem(""))
+  $("#19 .schedule").val(localStorage.getItem(""))
+
+ // display current day on page
+ $("#currentDay").text(moment().format("dddd, MMMM Do"));
 });
+
+
+
+
+
+
+
+
 //THEN I can enter an event
 
 
